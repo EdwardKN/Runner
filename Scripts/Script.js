e087b507dd242e5564c553954223e58c7656f557
Edward <edward.klinghammer@gmail.com>
2020-12-16 08:54:13 +0100

    testing out background system

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -19,6 +19,9 @@ var cactusImg2 = new Image();
 var cactusImg3 = new Image();
 var cactusImg4 = new Image();
 
+var groundImg1 = new Image();
+var groundImg2 = new Image();
+
 var c = canvas.getContext('2d');
 c.imageSmoothingEnabled = false;
 
@@ -32,6 +35,12 @@ var menu = {
 var standard = {
     jumpspeed: 35,
     height: 640,
+}
+var back1 = {
+    x: 0,
+}
+var back2 = {
+    x: 1920
 }
 var cactus1 = {
     x: -2000,
@@ -67,9 +76,9 @@ var player = {
     jumpSpeed: standard.jumpspeed,
     gravitation: 2,
     crouch: false,
-    speed: 20,
+    speed: 24,
     dead: false,
-    crouchValue:8*8
+    crouchValue: 8 * 8
 };
 
 
@@ -92,7 +101,7 @@ window.addEventListener('keydown', function (event) {
     if (event.code === "Escape" && player.dead === false) {
         toggleMenu();
     }
-    if(event.code === "Enter" && player.dead === true){
+    if (event.code === "Enter" && player.dead === true) {
         revive();
     }
 });
@@ -203,14 +212,25 @@ function showObstacle() {
         cactusImg4.src = 'Images/Obstacles/cactus4.png';
     }
 }
-
+function showBackground() {
+    if (groundImg1.complete) {
+        c.drawImage(groundImg1, Math.floor(back1.x), Math.floor(standard.height + 256), 1920, 640);
+        groundImg1.src = 'Images/Background/ground.png';
+    }
+    if (groundImg2.complete) {
+        c.drawImage(groundImg2, Math.floor(back2.x), Math.floor(standard.height + 256), 1920, 640);
+        groundImg2.src = 'Images/Background/ground.png';
+    }
+}
 function update() {
     c.fillStyle = 'white';
     c.fillRect(0, 0, canvas.width, canvas.height);
     showPlayer();
     showObstacle();
+    showBackground();
     checkAir();
     moveObstacle();
+    moveBackground();
     checkCollision();
     showDebugMenu();
 }
@@ -218,10 +238,10 @@ function update() {
 function showDebugMenu() {
     if (settings.debug === true) {
         c.fillStyle = "rgba(0, 0, 0, 0.5)";
-        if(player.crouch === false){
-            c.fillRect(player.x + 8*5, player.y + 8 * 9, 8 * 19, 8 * 23);
-        }else{
-            c.fillRect(player.x + 8*5, player.y + 8 * 9+player.crouchValue, 8 * 19, 8 * 23-player.crouchValue);
+        if (player.crouch === false) {
+            c.fillRect(player.x + 8 * 5, player.y + 8 * 9, 8 * 19, 8 * 23);
+        } else {
+            c.fillRect(player.x + 8 * 5, player.y + 8 * 9 + player.crouchValue, 8 * 19, 8 * 23 - player.crouchValue);
         }
 
         c.fillStyle = "rgba(0, 0, 0, 0.5)";
@@ -234,44 +254,44 @@ function showDebugMenu() {
         c.fillRect(cactus4.x + 8 * 11, cactus4.y + 8 * 12, 8 * 13, 8 * 20);
 
         c.fillStyle = "rgba(0, 0, 0, 0.5)";
-        c.fillRect(bird.x + 8*4, bird.y + 8 * 8, 8 * 22, 8 * 10);
+        c.fillRect(bird.x + 8 * 4, bird.y + 8 * 8, 8 * 22, 8 * 10);
     }
 }
 function checkCollision() {
-    if (player.x + 8*5 + 8 * 19 > cactus1.x + 8 * 11 && player.x + 8*5 < cactus1.x + 8 * 11 + 8 * 13 && player.y > standard.height - 100 && menu.pause === false) {
-        if(player.x < cactus1.x+8*5){
+    if (player.x + 8 * 5 + 8 * 19 > cactus1.x + 8 * 11 && player.x + 8 * 5 < cactus1.x + 8 * 11 + 8 * 13 && player.y > standard.height - 100 && menu.pause === false) {
+        if (player.x < cactus1.x + 8 * 5) {
             cactus1.x -= 64;
         }
         die();
     }
     if (player.x + 8 + 8 * 23 > cactus2.x + 8 * 11 && player.x + 8 < cactus2.x + 8 * 11 + 8 * 13 && player.y > standard.height - 100 && menu.pause === false) {
-        if(player.x < cactus2.x+8*5){
+        if (player.x < cactus2.x + 8 * 5) {
             cactus2.x -= 64;
-        }        
+        }
         die();
     }
     if (player.x + 8 + 8 * 23 > cactus3.x + 8 * 11 && player.x + 8 < cactus3.x + 8 * 11 + 8 * 13 && player.y > standard.height - 100 && menu.pause === false) {
-        if(player.x < cactus2.x+8*5){
+        if (player.x < cactus2.x + 8 * 5) {
             cactus2.x -= 64;
-        }        
+        }
         die();
     }
     if (player.x + 8 + 8 * 23 > cactus4.x + 8 * 11 && player.x + 8 < cactus4.x + 8 * 11 + 8 * 13 && player.y > standard.height - 100 && menu.pause === false) {
-        if(player.x < cactus2.x+8*5){
+        if (player.x < cactus2.x + 8 * 5) {
             cactus2.x -= 64;
-        }        
+        }
         die();
     }
-    if (player.x + 8 + 8 * 23 > bird.x + 8 * 4 && player.x + 8 < bird.x + 8 * 4 + 8 * 22 && player.y < bird.y+8*4+8*10 && player.y+8*9+8*23 > bird.y+8*4 && menu.pause === false && player.crouch === false) {
+    if (player.x + 8 + 8 * 23 > bird.x + 8 * 4 && player.x + 8 < bird.x + 8 * 4 + 8 * 22 && player.y < bird.y + 8 * 4 + 8 * 10 && player.y + 8 * 9 + 8 * 23 > bird.y + 8 * 4 && menu.pause === false && player.crouch === false) {
         cactus4.x -= 64;
         die();
     }
-    if (player.x + 8 + 8 * 23 > bird.x + 8 * 4 && player.x + 8 < bird.x + 8 * 4 + 8 * 22 && player.y+player.crouchValue < bird.y+8*4+8*10 && player.y+8*9+8*23 > bird.y+8*4 && menu.pause === false && player.crouch === true) {
+    if (player.x + 8 + 8 * 23 > bird.x + 8 * 4 && player.x + 8 < bird.x + 8 * 4 + 8 * 22 && player.y + player.crouchValue < bird.y + 8 * 4 + 8 * 10 && player.y + 8 * 9 + 8 * 23 > bird.y + 8 * 4 && menu.pause === false && player.crouch === true) {
         cactus4.x -= 64;
         die();
     }
 }
-function unPause(){
+function unPause() {
     menu.pause = false;
     player.animationState = 1;
 }
@@ -281,7 +301,7 @@ function die() {
     menu.pause = true;
 }
 
-function revive(){
+function revive() {
     cactus1 = {
         x: -2000,
         y: standard.height
@@ -298,15 +318,15 @@ function revive(){
         x: -2000,
         y: standard.height
     };
-    
+
     bird = {
         x: -2000,
         y: (standard.height + 100) - Math.random() * 300,
         animationState: 1,
         animation: 1,
-        speed:1.25
+        speed: 1.25
     };
-    
+
     player = {
         x: 200,
         y: standard.height,
@@ -316,19 +336,32 @@ function revive(){
         jumpSpeed: standard.jumpspeed,
         gravitation: 2,
         crouch: false,
-        speed: 20,
+        speed: 24,
         dead: false,
-        crouchValue:8*8
-    };    
+        crouchValue: 8 * 8
+    };
 }
 
 function moveObstacle() {
     if (menu.pause === false) {
-        bird.x -= player.speed*bird.speed;
+        bird.x -= player.speed * bird.speed;
         cactus1.x -= player.speed;
         cactus2.x -= player.speed;
         cactus3.x -= player.speed;
         cactus4.x -= player.speed;
+
+    }
+}
+function moveBackground() {
+    if (menu.pause === false) {
+        if (back1.x < -1920+player.speed) {
+            back1.x = 1920 - player.speed;
+        }
+        if (back2.x < -1920+player.speed) {
+            back2.x = 1920 - player.speed;
+        }
+        back1.x -= player.speed;
+        back2.x -= player.speed;
     }
 }
 
@@ -340,7 +373,7 @@ function checkAir() {
     } else {
         if (player.crouch === false && player.animationState !== 4 && player.animationState !== 5) {
             player.animationState = 1;
-        } else if(player.animationState !== 4 && player.animationState !== 5){
+        } else if (player.animationState !== 4 && player.animationState !== 5) {
             player.animationState = 3;
         }
         player.inAir = false;
@@ -377,7 +410,7 @@ function crouchEnd() {
 
 function teleport() {
     if (menu.pause === false) {
-        var chosen = Math.floor(Math.random()*5);
+        var chosen = Math.floor(Math.random() * 5);
         console.log(chosen)
         if (cactus1.x < -200 && chosen === 0) {
             cactus1.x = 2000 + (Math.random() * 300);
@@ -393,7 +426,7 @@ function teleport() {
         }
         else if (bird.x < -200 && chosen === 4) {
             bird.x = 2000 + (Math.random() * 300);
-            bird.y = (standard.height + 100)-(Math.random() * 200);
+            bird.y = (standard.height + 100) - (Math.random() * 200);
             console.log("bra")
         }
         else {
@@ -416,14 +449,15 @@ setInterval(function () {
 }, 100);
 
 setInterval(function () {
-
-    if (bird.animation === 1) {
-        bird.animation = 2;
-        return;
-    }
-    if (bird.animation === 2) {
-        bird.animation = 1;
-        return;
+    if (menu.pause === false) {
+        if (bird.animation === 1) {
+            bird.animation = 2;
+            return;
+        }
+        if (bird.animation === 2) {
+            bird.animation = 1;
+            return;
+        }
     }
 }, 200);
 
