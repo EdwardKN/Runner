0c30d89b9585785674cd1d05fa5e31b715556e97
EdwardKN <edward.klinghammer@gmail.com>
2021-09-15 10:16:29 +0200

    man kan croucha längre

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -356,7 +356,7 @@ window.addEventListener('keydown', function (event) {
             }
 
             if (event.code === "ControlLeft" || event.code === "ArrowDown") {
-                if (menu.pause === false && player.dead === false && player.crouchCooldownValue < 25) {
+                if (menu.pause === false && player.dead === false && player.crouchCooldownValue < 35) {
                     crouch();
                 }
             }
